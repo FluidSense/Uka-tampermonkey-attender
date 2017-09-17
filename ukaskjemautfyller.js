455e835c15776b8716b91f9559ba07d0b2866529
Johannes Kvamme <johanneskv96@gmail.com>
2017-09-17 13:03:29 +0200

    Update ukaskjemautfyller.js

--- ukaskjemautfyller.js
+++ ukaskjemautfyller.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Bestiller oktoberfestbilletter
-// @author       You
+// @author       Johannes Kvamme
 // @match        https://uka.no/program/377-oktoberfest/377/billetter
 // @grant        none
 // ==/UserScript==
