6360480aac9623bc0e7c4ab11280a9e5a231efde
Johannes Kvamme <johanneskv96@gmail.com>
2017-09-17 13:14:07 +0200

    Update ukaskjemautfyller.js

--- ukaskjemautfyller.js
+++ ukaskjemautfyller.js
@@ -12,8 +12,7 @@
     'use strict';
 
     new MutationObserver(function(mutations) {
-    // check at least two H1 exist using the extremely fast getElementsByTagName
-    // which is faster than enumerating all the added nodes in mutations
+    // Checks for a purchase button by ID before executing, to prevent script-run before all fields are available.
     if (document.getElementById('purchase-btn')) {
         document.getElementsByTagName('SELECT')[0].value = "4";document.getElementById('form_email').value = "test@gmail.com";document.getElementById('form_ccno').value="1122334455667788";document.getElementById('form_exp_month').value="01";document.getElementById('form_exp_year').value="18";document.getElementById('form_cvc2').value="010";document.getElementById('purchase-btn').click();
     }
